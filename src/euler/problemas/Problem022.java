db494d7aa242ba1b93e9dfd15c3d4674cafd2d04
Jose <jmverde@gmail.com>
2015-07-28 00:25:06 +0200

    Cambio de ciudad

---
+++ src/euler/problemas/Problem022.java
@@ -0,0 +1,22 @@
+package euler.problemas;
+
+import euler.comunes.ISolucion;
+
+public class Problem022 implements ISolucion {
+
+	
+	public static void main(String[] args) {
+		Problem022 p22 = new Problem022();
+		String resolver = p22.resolver();
+		System.out.println(resolver);
+
+	}
+	
+	
+	@Override
+	public String resolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
